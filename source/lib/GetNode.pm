405943be7ce54d7cc1df48c3f0e0f7f5276b07ed
shiro <shiro.mns@gmail.com>
2018-09-09 17:03:33 +0900

    [追加]未定義のノードを探索したときにエラーで止まらないように

--- source/lib/GetNode.pm
+++ source/lib/GetNode.pm
@@ -19,12 +19,46 @@ sub GetNode_Tag_CellPadding {
     
     my $return_nodes    = [];
     
+    if (!$$node) {
+        print "Warning: Use of uninitialized node [GetNode_Tag_CellPadding:$tag_name, $attr_cellpadding]\n";
+        return $return_nodes;
+    }
+
     #各メニュー情報の抜出
     @$return_nodes = $$node->look_down(
                 _tag => $tag_name,
                 sub {
                     if( $_[0]->attr('cellpadding')){
                         $_[0]->attr('cellpadding') eq $attr_cellpadding
+                    }
+                }
+    );
+    return $return_nodes;
+}
+
+#-----------------------------------#
+#    ノードの取得
+#-----------------------------------#
+#    引数｜タグ名、width
+#-----------------------------------#
+sub GetNode_Tag_Width {
+    my $tag_name        = shift;
+    my $attr_width      = shift;
+    my $node            = shift;
+    
+    my $return_nodes    = [];
+    
+    if (!$$node) {
+        print "Warning: Use of uninitialized node [GetNode_Tag_Width:$tag_name, $attr_width]\n";
+        return $return_nodes;
+    }
+
+    #各メニュー情報の抜出
+    @$return_nodes = $$node->look_down(
+                _tag => $tag_name,
+                sub {
+                    if( $_[0]->attr('width')){
+                        $_[0]->attr('width') eq $attr_width
                     }
                 }
     );
@@ -43,6 +77,11 @@ sub GetNode_Tag_Color {
     
     my $return_nodes    = [];
     
+    if (!$$node) {
+        print "Warning: Use of uninitialized node [GetNode_Tag_Color:$tag_name, $attr_color]\n";
+        return $return_nodes;
+    }
+
     #各メニュー情報の抜出
     @$return_nodes = $$node->look_down(
                 _tag => $tag_name,
@@ -68,6 +107,11 @@ sub GetNode_Tag_Id {
     
     my $return_nodes    = [];
     
+    if (!$$node) {
+        print "Warning: Use of uninitialized node [GetNode_Tag_Id:$tag_name, $attr_id]\n";
+        return $return_nodes;
+    }
+
     #各メニュー情報の抜出
     @$return_nodes = $$node->look_down(
                 _tag => $tag_name,
@@ -92,6 +136,11 @@ sub GetNode_Tag_Name {
     
     my $return_nodes    = [];
     
+    if (!$$node) {
+        print "Warning: Use of uninitialized node [GetNode_Tag_Name:$tag_name, $attr_name]\n";
+        return $return_nodes;
+    }
+
     #各メニュー情報の抜出
     @$return_nodes = $$node->look_down(
                 _tag => $tag_name,
@@ -117,6 +166,11 @@ sub GetNode_Tag_Class {
     
     my $return_nodes    = [];
     
+    if (!$$node) {
+        print "Warning: Use of uninitialized node [GetNode_Tag_Class:$tag_name, $attr_class]\n";
+        return $return_nodes;
+    }
+
     #各メニュー情報の抜出
     @$return_nodes = $$node->look_down(
                 _tag => $tag_name,
@@ -142,6 +196,11 @@ sub GetNode_Tag_ClassName {
     
     my $return_nodes    = [];
     
+    if (!$$node) {
+        print "Warning: Use of uninitialized node [GetNode_Tag_ClassName:$tag_name, $attr_class, $attr_name]\n";
+        return $return_nodes;
+    }
+
     #各メニュー情報の抜出
     @$return_nodes = $$node->look_down(
                 _tag => $tag_name,
@@ -165,6 +224,11 @@ sub GetNode_Tag {
     
     my $return_nodes            = [];
     
+    if (!$$node) {
+        print "Warning: Use of uninitialized node [GetNode_Tag:$tag_name]\n";
+        return $return_nodes;
+    }
+
     #各メニュー情報の抜出
     @$return_nodes = $$node->look_down(
                 _tag => $tag_name,
