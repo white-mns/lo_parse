e1b0f52d4032a9bfc457b86469ade34cd6db9f82
shiro <shiro.mns@gmail.com>
2018-09-22 11:11:20 +0900

    [リファクタリング]位置合わせ

--- ConstData.pm
+++ ConstData.pm
@@ -21,7 +21,7 @@ use warnings;
         use constant FLAGMENT_START    => 1;    #部分探索開始
         use constant FLAGMENT_END      => 1;    #部分探索終了
     
-    use constant EXE_CHARA => 1;
+    use constant EXE_CHARA                => 1;
         use constant EXE_CHARA_NAME               => 1;
         use constant EXE_CHARA_PROFILE            => 1;
         use constant EXE_CHARA_SUBJECT            => 1;
@@ -35,6 +35,6 @@ use warnings;
         use constant EXE_CHARA_PLACE              => 1;
         use constant EXE_CHARA_DEVELOPMENT_RESULT => 1;
         use constant EXE_CHARA_TRAINING           => 1;
-    use constant EXE_BATTLE => 1;
-        use constant EXE_BATTLE_CARD_USE => 1;
+    use constant EXE_BATTLE               => 1;
+        use constant EXE_BATTLE_CARD_USE          => 1;
 1;
