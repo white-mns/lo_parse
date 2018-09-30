398f6214df93af29e2a2fcb4451bc34ebe759896
shiro <shiro.mns@gmail.com>
2018-09-30 21:52:32 +0900

    [修正]一括再解析のLZHファイル名の命名規則が間違っている

--- _execue_all.sh
+++ _execue_all.sh
@@ -5,13 +5,8 @@ cd `dirname $0`	#解析コードのあるディレクトリで作業をする
 
 for ((RESULT_NO=$1;RESULT_NO <= $2;RESULT_NO++)) {
     for ((GENERATE_NO=5;GENERATE_NO >=0;GENERATE_NO--)) {
-        RESULT_NO0=`printf "%03d" $RESULT_NO`
         
-        if [ $GENERATE_NO -eq 0 ]; then
-            LZH_NAME=${RESULT_NO0}
-        else
-            LZH_NAME=${RESULT_NO0}_$GENERATE_NO
-        fi
+        LZH_NAME=${RESULT_NO}_$GENERATE_NO
 
         if [ -f ./data/orig/result${LZH_NAME}.lzh ]; then
             echo "start $LZH_NAME"
